3ce4a41845ccd872c0e93daad2e9349e32699b8c
Ilia Turygin <turygin_ia@mail.ru>
2025-06-24 20:07:51 +0300

    completed 5 assignments on the project

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,31 @@
 import prompt
+
 from random import randint
 from brain_games.cli import welcome_user
 
 
 def main():
-    welcome_user()
-    name = prompt.string('Answer "yes" if the number is even, otherwise answer "no".')
+    name = welcome_user()
+    rules()
+    correct_answers = 0
+
+    while correct_answers < 3:
+        random_number = randint(1, 100)
+        print(f'Question: {random_number}')
+        answer = input('Your answer: ')
+
+        if (random_number % 2 == 0 and answer == 'yes') or (random_number % 2 != 0 and answer == 'no'):
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {'yes' if random_number % 2 == 0 else 'no'}.")
+            print(f"Let's try again, {name}!")
+            correct_answers = 0  # Сброс счетчика правильных ответов
+    
+    print(f'Congratulations, {name}!')
 
 
 
+def rules():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
